c2864c646b605f61ab8a1ebb4efd16b4aad29b93
Sachin Chourasiya <sachinchourasiya.c.123@gmail.com>
2021-11-28 17:16:39 +0530

    Meals Finder Files

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+
+searchBtn.addEventListener('click', getMealList);
+
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+}
